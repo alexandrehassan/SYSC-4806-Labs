c4c44b399b6917eae247615ce9602ea3fbc307ba
Alexandre Hassan <alexhassan@live.ca>
2022-01-26 22:01:03 -0500

    BuddyInfo persisted with tests

--- src/main/java/BuddyInfo.java
+++ src/main/java/BuddyInfo.java
@@ -1,26 +1,74 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
- * Class made for SYSC 4806 course lab 1 to store a buddy's information
+ * Class made for SYSC 4806 course lab 2 to store a buddy's information
  *
  * @author Alexandre Hassan
- * @version january 20, 2022
+ * @version january 26, 2022
  */
+@Entity
 public class BuddyInfo{
 
+    private int id;
     private String name;
     private String phoneNumber;
 
 
+    /**
+     * Creates an instance of BuddyInfo with empty name and number
+     */
+    public BuddyInfo(){
+        this.name="";
+        this.phoneNumber="";
+    }
+
+    /**
+     * Creates an instance of BuddyInfo with given name and number
+     * @param name the name of the buddy
+     * @param phoneNumber the phone number of the buddy
+     */
     public BuddyInfo(String name, String phoneNumber){
         this.name=name;
         this.phoneNumber=phoneNumber;
     }
 
+    /**
+     * Gets the id of this Player. The persistence provider should
+     * autogenerate a unique id for new player objects.
+     * @return the id
+     */
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Sets the id of this Player to the specified value.
+     * @param id the new id
+     */
+    public void setId(int id){
+        this.id=id;
+    }
+
+
     public String getName(){
         return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
     }
 
     public String getPhoneNumber(){
         return phoneNumber;
     }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.name=phoneNumber;
+    }
+
 
 }
--- src/test/java/BuddyInfoTest.java
+++ src/test/java/BuddyInfoTest.java
@@ -1,5 +1,8 @@
-import org.junit.Before;
 import org.junit.Test;
+
+import javax.persistence.*;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -22,4 +25,56 @@ public class BuddyInfoTest{
         BuddyInfo buddyInfo = new BuddyInfo("Name", "413");
         assertEquals(buddyInfo.getPhoneNumber(), "413");
     }
+
+    @Test
+    public void persistence(){
+        // Connecting to the database through EntityManagerFactory
+        // connection details loaded from persistence.xml
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Lab");
+        EntityManager em = emf.createEntityManager();
+
+
+        int testID = addTestObjects(em);
+        List<BuddyInfo> results = queryTestObjects(em);
+
+        assertEquals("Query returned more than one object",1,results.size());
+        BuddyInfo testBuddy = results.get(0);
+
+        assertEquals("TestObject",testBuddy.getName());
+        assertEquals("TestNumber",testBuddy.getPhoneNumber());
+        assertEquals(testID, testBuddy.getId());
+        removeTestObjects(em,testBuddy);
+
+        results = queryTestObjects(em);
+        assertEquals("Object not removed correctly",0,results.size());
+    }
+
+
+
+    private void removeTestObjects(EntityManager em, BuddyInfo testBuddy){
+        em.getTransaction().begin();
+        em.remove(testBuddy);
+        em.getTransaction().commit();
+    }
+
+    private int addTestObjects(EntityManager em){
+        BuddyInfo buddyInfo = new BuddyInfo("TestObject","TestNumber");
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        em.persist(buddyInfo);
+
+        tx.commit();
+        return buddyInfo.getId();
+    }
+
+    private List<BuddyInfo> queryTestObjects(EntityManager em){
+        // Querying the contents of the database using JPQL query
+        Query q = em.createQuery("SELECT b FROM BuddyInfo b where b.name LIKE 'TestObject%'");
+
+        return q.getResultList();
+    }
+
+
 }
