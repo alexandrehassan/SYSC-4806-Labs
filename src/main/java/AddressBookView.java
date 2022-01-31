569344aba9e81a7c3e3654843296fb29469b7072
Alexandre Hassan <alexhassan@live.ca>
2022-01-31 10:21:23 -0500

    reduce size of view class

--- src/main/java/AddressBookView.java
+++ src/main/java/AddressBookView.java
@@ -2,14 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AddressBookView extends JFrame{
+    protected int width;
+    protected int height;
 
     public void init(){
-        // Set up the frame
-        setLayout(new GridLayout(3,2,20,20));
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setSize(400, 400);
-
         setVisible(true);
     }
-
 }
