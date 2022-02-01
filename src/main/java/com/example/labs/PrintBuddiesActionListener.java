ea7cb22cba624f755f95d515f92e83e9af1e7997
Alexandre Hassan <alexhassan@live.ca>
2022-01-31 20:40:30 -0500

    Remove UI stuff

--- src/main/java/com/example/labs/PrintBuddiesActionListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.labs;
-
-import java.awt.event.ActionEvent;
-
-public class PrintBuddiesActionListener extends AddressBookActionListener{
-    public void actionPerformed(ActionEvent e){
-        model.printBuddies();
-    }
-}
