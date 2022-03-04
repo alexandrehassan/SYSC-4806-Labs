fda7f86b88d7232861cae8a04a7866fc22dc18b2
Alexandre Hassan <alexhassan@live.ca>
2022-03-04 14:09:32 -0500

    Removing stuff

--- src/main/java/com/example/spring/AddressBookController.java
+++ src/main/java/com/example/spring/AddressBookController.java
@@ -22,22 +22,13 @@ public class AddressBookController {
     @Autowired
     BuddyInfoRepository buddyInfoRepository;
 
-    /**
-     * Controller to create a new addressbook using a POST request using JSON
-     * Used in js ajax call
-     * can be run using a curl or http request, an example request can be found in the http folder "NewAddressBook"
-     */
+
     @PostMapping("/AddressBook/new")
     public void newAddressBook(@RequestBody AddressBook book) {
         addressBookRepository.save(book);
     }
 
-    /**
-     * Controller to create a new buddy in an addressbook using a POST request using JSON
-     * Can only be used once an addressbook has been made
-     * used in js ajax call
-     * can be run using a curl or http request, an example request can be found in the http folder "AddBuddy"
-     */
+
     @PostMapping("/AddressBook/addBuddy")
     public void addBuddy(@RequestBody BuddyInfo buddy) {
         AddressBook book = addressBookRepository.findAddressBookById(buddy.getAddressBookId());
@@ -46,65 +37,4 @@ public class AddressBookController {
         addressBookRepository.save(book);
     }
 
-    /**
-     * Controller to show New Address Book Page form
-     *
-     */
-    @GetMapping("/newAddressBook")
-    public String newAddressBookForm(Model model) {
-        model.addAttribute("addressbook", new AddressBook());
-        return "newaddressbook";
-    }
-
-    /**
-     *Controller used on new address book form submission to save new book and show all saved books
-     */
-    @PostMapping("/newAddressBook")
-    public String newAddressBookSubmit(@ModelAttribute AddressBook addressBook, Model model) {
-        addressBookRepository.save(addressBook);
-        model.addAttribute("addressbooks", addressBookRepository.findAll());
-        model.addAttribute("addressbook", addressBook);
-        return "newaddressbook";
-    }
-
-    /**
-     *Controller used to show view address book page
-     * gets saved address books and gives to model to show
-     */
-    @GetMapping("/viewAddressBooks")
-    public String viewAddressBooks(Model model) {
-        model.addAttribute("addressbooks", addressBookRepository.findAll());
-        return "viewaddressbooks";
-    }
-
-    /**
-     *Controller to show add buddy form
-     * Auto shows first address book to display, but buddy can be added to any saved addressbook using the aBID
-     */
-    @GetMapping("/addBuddy")
-    public String addBuddyForm(Model model) {
-        AddressBook addressBook = addressBookRepository.findAddressBookById(1L);
-        if (addressBook == null) {
-            addressBook = new AddressBook("Address Book");
-        }
-        BuddyInfo buddy = new BuddyInfo();
-        model.addAttribute("addressbook", addressBook);
-        model.addAttribute("buddy", buddy);
-        return "addbuddy";
-    }
-
-    /**
-     * Controller to submit add buddy form
-     * saves new buddy, gets the address book it is to be added to, adds it, saves addressbook, and displays the buddy and addressbook
-     */
-    @PostMapping("/addBuddy")
-    public String addBuddySubmit(@ModelAttribute BuddyInfo buddyInfo, Model model) {
-        buddyInfoRepository.save(buddyInfo);
-        AddressBook book = addressBookRepository.findAddressBookById(buddyInfo.getAddressBookId());
-        book.addBuddy(buddyInfo);
-        addressBookRepository.save(book);
-        model.addAttribute("buddy", buddyInfo);
-        model.addAttribute("addressbook", book);
-        return "addbuddy";
-    }
 }
